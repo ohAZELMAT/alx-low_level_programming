adca3778cf0459cb00705addcaafaa29bc1ffda1
ohAZELMAT <othmaneazelmat@gmail.com>
2023-10-12 12:15:44 -0700

    cs:go

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,36 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
-int main(void)
+void print_triangle(int size)
 {
-        int i;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
-        for (i = 1; i <= 100; i++)
-        {
-                if (i % 3 == 0 && i % 5 != 0)
-                {
-                        printf(" Fizz");
-                } else if (i % 5 == 0 && i % 3 != 0)
-                {
-                        printf(" Buzz");
-                } else if (i % 3 == 0 && i % 5 == 0)
-                {
-                        printf(" FizzBuzz");
-                } else if (i == 1)
-                {
-                        printf("%d", i);
-                } else
-                {
-                        printf(" %d", i);
-                }
-        }
-        printf("\n");
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
 
-        return (0);
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
 }
